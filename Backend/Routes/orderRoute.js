fd1305dc2e60d87e7e26a57e128a937578ece660
rahulrc1525 <rahulrc1525@gmail.com>
2025-02-08 18:12:39 +0530

    changes in placeOrder

--- Backend/Routes/orderRoute.js
+++ Backend/Routes/orderRoute.js
@@ -1,14 +1,16 @@
-import express from "express"
-import authMiddleware from "../middleware/auth.js"
-import { placeOrder, verifyOrder,userOrders ,listOrders,updateStatus} from "../controllers/orderController.js"
+import express from "express";
+import authMiddleware from "../middleware/auth.js";
+import { placeOrder, verifyOrder,userOrders, listOrders, updateStatus, } from "../Controllers/orderController.js";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/place",authMiddleware,placeOrder);
-orderRouter.post("/verify",verifyOrder)
-orderRouter.post("/userorders",authMiddleware,userOrders)//to convert auth token into middle ware
-orderRouter.get("/list",listOrders)
-orderRouter.post("/status",updateStatus)
+// Route to place an order (protected by authentication middleware)
+orderRouter.post("/place", authMiddleware, placeOrder);
 
+// Route to verify Razorpay payment
+orderRouter.post("/verify", verifyOrder);
+orderRouter.post("/userOrders", userOrders);
+orderRouter.post("/listOrders", listOrders);
+orderRouter.post("/updateStatus", updateStatus);
 
 export default orderRouter;
