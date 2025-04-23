d5f9ab220105a0db8e6c0f21712d2a72d7edc2ed
rahulrc1525 <rahulrc1525@gmail.com>
2025-04-24 01:48:33 +0530

    email auth

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
 import userController from "../Controllers/userController.js";
+const { loginUser, registerUser, verifyEmail, forgotPassword, resetPassword } = userController;
 
 const userRouter = express.Router();
 
-userRouter.post("/register", userController.registerUser);
-userRouter.post("/login", userController.loginUser);
-userRouter.get("/verify-email", userController.verifyEmail);
-userRouter.post("/forgot-password", userController.forgotPassword);
-userRouter.post("/reset-password", userController.resetPassword);
-userRouter.post("/refresh-token", userController.refreshToken);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/verify-email", verifyEmail);
+userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password", resetPassword);
 
 export default userRouter;
